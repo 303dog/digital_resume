18a3938fd6c033a89ea573adf2ae4d832a4b47e9
Paul_Beckwith <303dog@gmail.com>
2020-11-22 15:27:30 -0700

    render json:

--- app/controllers/work_exps_controller.rb
+++ app/controllers/work_exps_controller.rb
@@ -1,4 +1,13 @@
 class WorkExpsController < ApplicationController
 
+    def index 
+        workx = WorkExp.all 
+        render json: workx
+    end
+
+    def show 
+        work = WorkExp.find_by(id: params[:id])
+        render json: work 
+    end
     
 end
